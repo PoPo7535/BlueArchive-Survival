cf31d16834b8d7cccb3c3df843d78c214502176d
PoPo <dltngus7535@gmail.com>
2024-12-23 05:52:57 +0900

    플레이어 레디 & 스타트 작업 1

--- Assets/2.Script/LobbyRoomPanel.cs
+++ Assets/2.Script/LobbyRoomPanel.cs
@@ -1,23 +1,29 @@
+using System.Collections.Generic;
 using System.Linq;
 using Fusion;
 using Sirenix.OdinInspector;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using Utility;
 
-public class LobbyRoomPanel : FusionSingleton<LobbyRoomPanel>, ISetInspector, IPlayerJoined
+public class LobbyRoomPanel : FusionSingleton<LobbyRoomPanel>, ISetInspector, IPlayerJoined, IPlayerLeft
 {
     public CanvasGroup cg;
     [SerializeField] private Button readyBtn;
+    [SerializeField] private TMP_Text readyText;
     [SerializeField] private Button cancelBtn;
     public PlayerSlot playerSlot;
+
+    private Dictionary<PlayerRef, bool> ready = new Dictionary<PlayerRef, bool>();
     [Button,GUIColor(0, 1, 0)]
     public void SetInspector()
     {
         var childs = transform.GetAllChild();
         cg = GetComponent<CanvasGroup>();
         readyBtn = childs.First(tr => tr.name == "Ready Btn").GetComponent<Button>();
+        readyText = childs.First(tr => tr.name == "Ready Text").GetComponent<TMP_Text>();
         cancelBtn = childs.First(tr => tr.name == "Cancel Btn").GetComponent<Button>();
         playerSlot = childs.First(tr => tr.name == "PlayerSlot").GetComponent<PlayerSlot>();
     }
@@ -38,10 +44,23 @@ public class LobbyRoomPanel : FusionSingleton<LobbyRoomPanel>, ISetInspector, IP
     {
         if (false == Object.HasStateAuthority)
             return;
-        var runner = App.I.runner;
-        runner.Spawn(GameManager.I.playerInfo, Vector3.zero, Quaternion.identity, player);
+        
+        App.I.runner.Spawn(GameManager.I.playerInfo, Vector3.zero, Quaternion.identity, player);
+        
+        if (Object.StateAuthority != player)
+            ready.Add(player, false);
+        
+        RefreshStartBtn();
     }
-
+    
+    public void PlayerLeft(PlayerRef player)
+    {
+        if (false == Object.HasStateAuthority)
+            return;
+        
+        if (Object.StateAuthority != player)
+            ready.Remove(player);
+    }
     
     public void SetSlot()
     {
@@ -53,4 +72,29 @@ public class LobbyRoomPanel : FusionSingleton<LobbyRoomPanel>, ISetInspector, IP
         for (int i = list.Count; i < 3; ++i)
             playerSlot.SetSlot(i);
     }
+
+    public override void Spawned()
+    {
+        if (Object.HasStateAuthority)
+        {
+            readyText.text = "Start";
+        }
+        else
+        {
+            readyText.text = "Ready";
+            
+        }
+    }
+
+    [Rpc(RpcSources.InputAuthority,RpcTargets.StateAuthority)]
+    private void RPC_InputReadyBtn(PlayerRef playerRef, bool isOn)
+    {
+        ready[playerRef] = isOn;
+    }
+
+    private void RefreshStartBtn()
+    {
+        readyBtn.interactable = false == ready.Values.Any(value => false == value);
+    }
+
 }
