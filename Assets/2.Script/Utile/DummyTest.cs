abe3a43ddad629e3b24f3febfff896175447cf16
PoPo <dltngus7535@gmail.com>
2025-03-26 05:52:44 +0900

    씬 수정 및 구글시트 로드

--- Assets/2.Script/Utile/DummyTest.cs
+++ Assets/2.Script/Utile/DummyTest.cs
@@ -1,4 +1,7 @@
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Cysharp.Threading.Tasks;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -9,27 +12,40 @@ using Fold = Sirenix.OdinInspector.FoldoutGroupAttribute;
 public class DummyTest : MonoBehaviour
 {
 
-    private string csvUrl = $"https://docs.google.com/spreadsheets/d/{API.GoogleSheetAPI}/gviz/tq?tqx=out:csv";
+    private static string csvUrl = $"https://docs.google.com/spreadsheets/d/{API.GoogleSheetAPI}/gviz/tq?tqx=out:csv";
 
-    void Start()
-    {
-        StartCoroutine(ReadCSV());
-    }
+
     
     [Button]
-    IEnumerator ReadCSV()
+    private static async void ReadCSV()
     {
-        using UnityWebRequest www = UnityWebRequest.Get(csvUrl);
-        yield return www.SendWebRequest();
+        using var www = UnityWebRequest.Get(csvUrl);
+        await www.SendWebRequest();
 
         if (www.result == UnityWebRequest.Result.Success)
         {
-            string csvData = www.downloadHandler.text;
-            Debug.Log("Received CSV: " + csvData);
+            var csvData = www.downloadHandler.text;
+            csvData.Log();
+            ParseCSV(csvData);
         }
         else
         {
             Debug.LogError("Error: " + www.error);
+        }
+    }
+
+    private static void ParseCSV(string csv, int x = 1, int y = 1)
+    {
+        var lines = csv.Split('\n');
+        lines.Length.Log();
+        foreach (var line in lines.Skip(y))
+        {
+            var values = line.Split(',');
+            values.Length.Log();
+            foreach (var val in values.Skip(x))
+            {
+                val.Log();
+            }
         }
     }
 
